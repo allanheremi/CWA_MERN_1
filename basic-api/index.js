b68869f9f7698866ecf0be3ea6755df25422a93c
Allan Heremi <allanheremidev@gmail.com>
2023-05-12 06:36:53 +0200

    Added notesList to UI and made routes connect correctly

--- basic-api/index.js
+++ basic-api/index.js
@@ -1,23 +1,28 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
 const port = 5000;
 
+app.use(cors());
+
 app.get('/', (request, response) => {
-response.send('Hello World!')
-})
+  response.send('Hello World!');
+});
 
 app.get('/notes', (request, response) => {
-    const notes = [{
-        text: 'Do homework!',
-        link: 'https://etherscan.io'
-
-    }, {
-        text: 'Buy NFTs',
-        link: 'https://Opensea.io'
-    }]
-    response.json({notes})
-    })
+  const notes = [
+    {
+      text: 'Do homework!',
+      link: 'https://etherscan.io',
+    },
+    {
+      text: 'Buy NFTs',
+      link: 'https://Opensea.io',
+    },
+  ];
+  response.json({ notes });
+});
 
 app.listen(port, () => {
-    console.log(`Notes backend is listening on port http://localhost:${port}`);
-})
+  console.log(`Notes backend is listening on port http://localhost:${port}`);
+});
